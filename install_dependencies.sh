b4ab6f7f820112fc2c24a55e36cd97881eb55b57
IKA-Syrian <computertk2018@gmail.com>
2024-07-30 05:36:56 +0300

    update the installation file

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,7 +2,7 @@
 
 # Update package list and install prerequisites
 sudo apt update
-sudo apt install -y curl gnupg software-properties-common gperf zlib1g-dev cmake g++ wget git
+sudo apt install -y curl gnupg software-properties-common gperf zlib1g-dev cmake g++ wget git libssl-dev openssl
 
 # Install Node.js 20.15.1 and PM2
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
@@ -17,9 +17,14 @@ sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
-# Install Telegram Bot API
-git clone https://github.com/tdlib/telegram-bot-api.git
+git clone --recurse-submodules https://github.com/tdlib/telegram-bot-api.git
 cd telegram-bot-api
+
+# Check if the 'td' directory exists and if not, initialize and update submodules
+if [ ! -d "td" ]; then
+    git submodule update --init --recursive
+fi
+
 mkdir build
 cd build
 cmake ..
